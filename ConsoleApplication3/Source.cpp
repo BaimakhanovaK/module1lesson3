64f76b4852e373a1991cb293cab02409c9ef80c9
БаймахановаК <@238_2>
2018-05-22 10:15:34 +0600

    Add project files.

---
+++ ConsoleApplication3/Source.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+
+
+
+
+
+}
+
+//int x = 0, y = 0;
+//
+//printf("введите значение х:");
+//scanf("%d", &x);
+
+//y = 7 * x*x - 3 * x + 6;
+
+//printf("y=%d \n", y);
+
+
+
+//float a = 0.0, S = 0.0;
+	//a = 8.5; 
+
+
+	//S = a*a;
+
+	//printf("S=%f\n", S);
